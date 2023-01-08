e4b969f5ba075b90e036808dd5850475d776a238
Jesse Sopel <jesse.sopel@gmail.com>
2023-01-08 16:05:14 -0500

    Test LED as Light entity

--- custom_components/ember_mug/services.py
+++ custom_components/ember_mug/services.py
@@ -9,7 +9,7 @@ from homeassistant.core import ServiceCall
 from homeassistant.helpers import config_validation as cv
 import voluptuous as vol
 
-from .const import ATTR_RGB_COLOR, ATTR_TARGET_TEMP, MUG_NAME_REGEX
+from .const import ATTR_TARGET_TEMP, MUG_NAME_REGEX
 
 if TYPE_CHECKING:
     from .sensor import EmberMugSensor
@@ -17,12 +17,6 @@ if TYPE_CHECKING:
 
 _LOGGER = logging.getLogger(__name__)
 
-SET_LED_COLOUR_SCHEMA = {
-    vol.Required(ATTR_RGB_COLOR): vol.All(
-        vol.ExactSequence((cv.byte,) * 3),
-        vol.Coerce(tuple),
-    ),
-}
 
 SET_TARGET_TEMP_SCHEMA = {
     vol.Required(ATTR_TARGET_TEMP): cv.positive_float,
@@ -31,13 +25,6 @@ SET_TARGET_TEMP_SCHEMA = {
 SET_MUG_NAME_SCHEMA = {
     vol.Required(ATTR_NAME): cv.matches_regex(MUG_NAME_REGEX),
 }
-
-
-async def set_led_colour(entity: EmberMugSensor, service_call: ServiceCall) -> None:
-    """Set LED colour of mug."""
-    led_colour: tuple[int, int, int] = service_call.data[ATTR_RGB_COLOR]
-    _LOGGER.info(f"Called service set led colour of {entity} to {led_colour})")
-    await entity.coordinator.connection.set_led_colour((*led_colour, 255))
 
 
 async def set_target_temp(entity: EmberMugSensor, service_call: ServiceCall) -> None:
