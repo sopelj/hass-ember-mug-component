124f8f65342e9ea5b202ace6ba39c1c39b88c510
Jesse Sopel <jesse.sopel@gmail.com>
2023-04-27 20:39:23 -0400

    Add volume level for Travel mug

--- custom_components/ember_mug/select.py
+++ custom_components/ember_mug/select.py
@@ -5,6 +5,7 @@ from enum import Enum
 import logging
 from typing import Literal
 
+from ember_mug.consts import VolumeLevel
 from homeassistant.components.select import SelectEntity, SelectEntityDescription
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import UnitOfTemperature
@@ -22,10 +23,16 @@ _LOGGER = logging.getLogger(__name__)
 TEMPERATURE_UNITS = [
     t.value for t in (UnitOfTemperature.CELSIUS, UnitOfTemperature.FAHRENHEIT)
 ]
+VOLUME_LEVELS = [v.value for v in VolumeLevel]
 SELECT_TYPES = {
     "temperature_unit": SelectEntityDescription(
         key="temperature_unit",
         options=TEMPERATURE_UNITS,
+        entity_category=EntityCategory.CONFIG,
+    ),
+    "volume_level": SelectEntityDescription(
+        key="volume_level",
+        options=VOLUME_LEVELS,
         entity_category=EntityCategory.CONFIG,
     ),
 }
@@ -73,6 +80,26 @@ class MugTempUnitSelectEntity(MugSelectEntity):
         await self.coordinator.mug.set_temperature_unit(option)
 
 
+class MugVolumeLevelSelectEntity(MugSelectEntity):
+    """Configurable SelectEntity for the travel mug volume level."""
+
+    @property
+    def icon(self) -> str:
+        """Change icon based on current option."""
+        if current := self.current_option:
+            return f"mdi:volume-{current}"
+        return "mdi:volume-off"
+
+    async def async_select_option(
+        self,
+        option: Literal["high", "medium", "low"] | VolumeLevel,
+    ) -> None:
+        """Change the selected option."""
+        if isinstance(option, str):
+            option = VolumeLevel(option)
+        await self.coordinator.mug.set_volume_level(option)
+
+
 async def async_setup_entry(
     hass: HomeAssistant,
     entry: ConfigEntry,
@@ -81,8 +108,11 @@ async def async_setup_entry(
     """Set up Select Entities."""
     assert entry.entry_id is not None
     data: HassMugData = hass.data[DOMAIN][entry.entry_id]
-    async_add_entities(
-        [
-            MugTempUnitSelectEntity(data.coordinator, "temperature_unit"),
-        ],
-    )
+    entities = [
+        MugTempUnitSelectEntity(data.coordinator, "temperature_unit"),
+    ]
+    if data.mug.data.model.is_travel_mug:
+        entities.append(
+            MugVolumeLevelSelectEntity(data.coordinator, "volume_level"),
+        )
+    async_add_entities(entities)
